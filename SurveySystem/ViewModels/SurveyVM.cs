814d427ee39d036837f96cb257c8523b5e13e9e4
PulinxDavy <davypulinx@hotmail.com>
2014-12-31 00:36:38 +0100

    Added User and Survey ViewModels

---
+++ SurveySystem/ViewModels/SurveyVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SurveySystem.Models;
+
+namespace SurveySystem.ViewModels
+{
+    public class SurveyVM
+    {
+        [Required(ErrorMessage = "A Survey Title is required")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Active { get; set; }
+
+        public string Comment { get; set; }
+
+        public virtual List<QuestionGroup> Groups { get; set; }
+
+        public virtual List<Question> Questions { get; set; }
+        
+    }
+}
