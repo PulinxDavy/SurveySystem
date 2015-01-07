4018d1abd63e82d14fbdc05efdb29f5cb4bb52d1
maximeryckmans <maximeryckmans@gmail.com>
2015-01-07 20:15:26 +0100

    QuestionGroup create

---
+++ SurveySystem/ViewModels/QuestionGroupVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SurveySystem.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurveySystem.ViewModels
+{
+    public class QuestionGroupVM
+    {
+        public string Title { get; set; }
+
+        [Display(Name = "Questions")]
+        public int SelectedQuestionId { get; set; }
+        public Question Question { get; set; }
+        public IEnumerable<string> SelectedQuestions { get; set; }
+        public IEnumerable<SelectListItem> Questions
+        {
+            get {
+                return new SelectList(AllQuestions, "Id", "QuestionString"); 
+            }
+        }
+        public List<Question> AllQuestions { get; set; }
+
+    }
+}
